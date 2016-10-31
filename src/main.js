feb87c5d6074c468b903e8b65821a9c5c474b884
connor50 <c-doyle@outlook.com>
2016-10-30 21:57:04 -0400

    framework for skimmed

---
+++ src/main.js
@@ -0,0 +1,29 @@
+
+var bbc = fetch('https://newsapi.org/v1/articles?source=bbc-news&sortBy=top&apiKey=6ef0dce6d16e45ffa1fdd01274f57bc2')
+.then(data => data.json());
+
+var dailymail = fetch('https://newsapi.org/v1/articles?source=daily-mail&sortBy=top&apiKey=6ef0dce6d16e45ffa1fdd01274f57bc2')
+.then(data => data.json());
+
+// load dictionary
+
+
+Promise.all([bbc, dailymail])
+.then(arrays => {
+  return arrays[0].articles.concat(arrays[1].articles);
+})
+.then(json => {
+  // //Sentiment Analysis and Description.
+  // sort by sentiment
+  // render out two lists
+  // hide list depending on bool value milk bottle
+  json.forEach(x => {
+    document.body.innerHTML+= `
+      <article>
+          <a href='${x.url}'><img src='${x.urlToImage}'></a>
+          <h2>${x.title}</h2>
+          <p>${x.description}</p>
+      </article>
+    `;
+  });
+});
